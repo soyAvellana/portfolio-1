ef5dab827546314f244656d7321c5112bbf40317
Hazel <hazelhmrcu@live.de>
2025-07-21 16:35:08 +0200

    Initial commit React + Tailwind Projekt

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+    </div>
+  );
+}
+export default App;
